789c5c434f228b16f1709a30e52fb905deb876df
Gelopaks <pak.ol@yahoo.com>
2023-03-15 15:55:29 +0700

    Finished with instanceof

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -4,21 +4,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Object o = new Scanner(System.in); //к переменной Object можноприсвоить объект любого класса
+        //Object o = new Scanner(System.in); //к переменной Object можноприсвоить объект любого класса
 
-        Scanner scanner = null; //инициализация
+        //Scanner scanner = null; //инициализация
 
-        if (o instanceof Scanner)scanner = (Scanner) o; //проверяет класс при преобразовании объекта возвращает true/false
+        //if (o instanceof Scanner)scanner = (Scanner) o; //проверяет класс при преобразовании объекта возвращает true/false
 
-        if (scanner != null){
-            scanner.nextInt(); // проверка
+        //if (scanner != null){
+        //  scanner.nextInt(); // проверка
+        //}
+
+        //Object o1 = new String(); //вкладываем другие типы данных
+
+        //Object o2 = new Integer(15); //вкладываем другие типы данных
+
+
+
+        Object[] objects = {10, "Привет", 3.14}; // здесь происходит автоупаковка
+
+        for (Object o : objects){
+            if (o instanceof String){
+                String s = (String) o;
+                System.out.println(s);
+            }
         }
 
-        Object o1 = new String(); //вкладываем другие типы данных
-
-        Object o2 = new Integer(15); //вкладываем другие типы данных
 
 
 
     }
+
+
+
 }
