9db797b8cf62da2fa83929741e4c349ed42b8ca6
Gelopaks <pak.ol@yahoo.com>
2023-03-19 01:06:19 +0700

    Method only generic implementation

---
+++ src/ru/synergy/genericmethods/Printer.java
@@ -0,0 +1,11 @@
+package ru.synergy.genericmethods;
+
+public class Printer {
+
+
+    public <T extends Object> void print(T[] items){
+        for (T item : items){
+            System.out.println(item);
+        }
+    }
+}
