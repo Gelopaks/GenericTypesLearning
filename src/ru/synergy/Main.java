857698ccf823220c8d45559f0e7fe9225bc0d128
Gelopaks <pak.ol@yahoo.com>
2023-03-18 19:27:27 +0700

    Adding the Out folder to .gitignore. Refactored list to integer generic type

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,55 +1,61 @@
 package ru.synergy;
-
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
-        //Object o = new Scanner(System.in); //к переменной Object можноприсвоить объект любого класса
 
-        //Scanner scanner = null; //инициализация
+// Casting
+        /*Object o = new Scanner(System.in); //к переменной Object можно присвоить объект любого класса
 
-        //if (o instanceof Scanner)scanner = (Scanner) o; //проверяет класс при преобразовании объекта возвращает true/false
+        //((Scanner) o ).nextInt(); // casting: явное преобразование объекта "o" класса Object
+                                    // в объект типа Scanner с использованием его методов
+        Object o1 = new String(); //вкладываем другие типы данных
+        Object o2 = new Integer(15); //вкладываем другие типы данных
 
-        //if (scanner != null){
-        //  scanner.nextInt(); // проверка
-        //}
+       Scanner scanner = null; //инициализация
+         if (o instanceof Scanner)scanner = (Scanner) o; //проверяет класс при преобразовании объекта возвращает true/false
 
-        //Object o1 = new String(); //вкладываем другие типы данных
-
-        //Object o2 = new Integer(15); //вкладываем другие типы данных
-
-
-
-        Object[] objects = {10, "Привет", 3.14}; // здесь происходит автоупаковка
-
-        for (Object o : objects){
-            if (o instanceof String){
+         if (scanner != null){   // проверяет результат преобразования
+           scanner.nextInt();  // ждет ввода данных типа Integer и после ввода завершает программу
+         }*/
+// Array
+        /*Object[] objects = {10, "Привет!", 3.14}; // здесь происходит автоупаковка
+                                                 // массива с разными типами данных
+        for (Object o : objects){  // цикл проверки принадлежности объектов
+            if (o instanceof String){ // в массиве к типу данных String
                 String s = (String) o;
                 System.out.println(s);
             }
+        }*/
+
+// ArrayList
+        ArrayList<Integer> numbers = new ArrayList(); //сохраняет Object. При объявлении типа данных в Object
+                                                      // сохраняется объект с объявленным типом данных,
+                                                      // что позволяе исключить проверку и преобразование объектов
+
+        for (int i = 0; i<10; i++){  //сохранять в коллекцию числа 10, 20 ...100
+            numbers.add(i*10);
+            //numbers.add(i*10.4f);
         }
 
+        //numbers.add(10.4f);
 
-        ArrayList numbers = new ArrayList(); //сохраняет Object
-
-        for (int i = 0; i<10; i++){
-            numbers.add(i*10); //сохранять в коллекцию числа 10, 20 ...100
-
-
-        }
         int sum = 0;
-        for (Object o : numbers){
+        for (Integer o : numbers){
             sum = sum + (Integer) o;
         }
 
         System.out.println(sum);
 
-
-
-
     }
 
-
-
 }
+/*
+*             то, что пишем мы в коде                          то, во что, преобразует компилятор
+* ArrayList<Integer> list = new ArrayList<Integer>();              ArrayList = new ArrayList
+*list.add(1);                                                      list.add((Integer) 1);
+* 
+*
+*
+*
+ */
